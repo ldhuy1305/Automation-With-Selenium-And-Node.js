8bc069479cb8d1ff7935e8133005ce8584599297
ldhuy1305 <bongmaxuyentuong1@gmail.com>
2023-11-27 23:14:30 +0700

    add features and decribe test case

--- test/homePage.test.js
+++ test/homePage.test.js
@@ -10,7 +10,7 @@ process.on("unhandledRejection", () => {});
 
 (async function example() {
   try {
-    describe("Google search automated testing", async function () {
+    describe("Search store in https://falth.vercel.app", async function () {
       this.timeout(50000);
       let driver, page;
 
@@ -25,49 +25,67 @@ process.on("unhandledRejection", () => {});
         await page.quit();
       });
 
-      // it("Find Ocha in search box and click search button", async () => {
-      //   const result = await page.submitKeywordAndGetResult("Ocha");
-      //   expect(result.length).to.be.equal(1);
-      // });
+      it("Search Och in search box and click search button", async () => {
+        const result = await page.submitKeywordAndGetResult("Och");
+        expect(result.length).to.be.equal(1);
+      });
 
-      // it("Find Ocha with s in search box and click search button", async () => {
-      //   const result = await page.submitKeywordAndGetResult("Ochas");
-      //   expect(result.length).to.be.equal(0);
-      // });
-      // it("Click search button", async () => {
-      //   const result = await page.clickCityAndGetResult("Hà Nội");
-      //   expect(result.length).to.be.equal(0);
-      // });
-      it("Click search button", async () => {
+      it("Search Ocha with s in search box and click search button", async () => {
+        const result = await page.submitKeywordAndGetResult("Ochas");
+        expect(result.length).to.be.equal(0);
+      });
+      it("Click 'Hà Nội' in drop-down city", async () => {
+        const result = await page.clickCityAndGetResult("Hà Nội");
+        expect(result.length).to.be.equal(0);
+      });
+      it("Click 'Đà Nẵng' in drop-down city and choose 'Quận Liên Chiểu' in drop-down local", async () => {
         const result = await page.clickDistrictAndGetResult(
-          "Quận Sơn Trà",
+          "Quận Liên Chiểu",
           "Đà Nẵng"
         );
         expect(result.length).to.be.above(0);
       });
-      // it("Click search button", async () => {
-      //   const result = await page.clickCategoryAndGetResult("Gà rán");
-      //   expect(result.length).to.be.above(0);
-      // });
-      // it("Click search button", async () => {
-      //   const result = await page.clickCategoryAndGetResult("Xúc xích");
-      //   expect(result.length).to.be.above(0);
-      // });
-      // it("Click search button", async () => {
-      //   const result = await page.clickCategoryAndGetResult("Xúc xích,Gà rán");
-      //   expect(result.length).to.be.above(0);
-      // });
-      // it("Click search button", async () => {
-      //   const result = await page.clickCategoryAndGetResult(
-      //     "Gà rán,Xúc xích,Thức uống"
-      //   );
-      //   expect(result.length).to.be.equal(0);
-      // });
-      // it("Click search button", async () => {
-      //   const result =
-      //     await page.clickCategoryCityAndGetResult("Gà rán,Đà Nẵng");
-      //   expect(result.length).to.be.equal(2);
-      // });
+      it("Click 'Đà Nẵng' in drop-down city then choose 'Quận Liên Chiểu' and 'Quận Sơn Trà' in drop-down local", async () => {
+        const result = await page.clickDistrictAndGetResult(
+          "Quận Liên Chiểu,Quận Sơn Trà",
+          "Đà Nẵng"
+        );
+        expect(result.length).to.be.above(0);
+      });
+      it("Click drop-down category and choose 'Xúc xích' and 'Gà rán'", async () => {
+        const result = await page.clickCategoryAndGetResult("Xúc xích,Gà rán");
+        expect(result.length).to.be.above(0);
+      });
+      it("Click drop-down category and choose 'Xúc xích','Gà rán' and 'Thức uống'", async () => {
+        const result = await page.clickCategoryAndGetResult(
+          "Gà rán,Xúc xích,Thức uống"
+        );
+        expect(result.length).to.be.equal(0);
+      });
+      it("Click 'Đà Nẵng' in drop-down city and choose 'Xúc xích','Gà rán' and 'Thức uống' in drop-down category ", async () => {
+        const result = await page.clickCategoryCityAndGetResult(
+          "Gà rán,Xúc xích",
+          "Đà Nẵng"
+        );
+        expect(result.length).to.be.equal(1);
+      });
+      it("Click 'Hà Nội' in drop-down city and then choose 'Quận Hoàng Mai' in drop-down local and choose 'Xúc xích','Gà rán' and 'Thức uống' in drop-down category ", async () => {
+        const result = await page.clickCategoryDistrictAndGetResult(
+          "Gà rán,Xúc xích",
+          "Quận Hoàng Mai",
+          "Hà Nội"
+        );
+        expect(result.length).to.be.equal(0);
+      });
+      it("Firt search with 'm',then choose 'Gà rán' drop-down category. after Click 'Đà Nẵng' in drop-down city and then choose 'Quận Sơn Trà','Quận Liên Chiểu' in drop-down local", async () => {
+        const result = await page.clickCategoryDistrictSearchAndGetResult(
+          "m",
+          "Gà rán",
+          "Quận Sơn Trà,Quận Liên Chiểu",
+          "Đà Nẵng"
+        );
+        expect(result.length).to.be.equal(0);
+      });
     });
   } catch (ex) {
     console.log(new Error(ex.message));
